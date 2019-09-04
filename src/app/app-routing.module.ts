20c50a8a5b79e4b42a9d36335ea926a4d8773961
rodriguez23 <ksrodriguez23@misena.edu.co>
2019-09-04 09:25:17 -0500

    Restaurante Proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'menu', loadChildren: './menu/menu.module#MenuPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
